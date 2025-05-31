76b6b7a3c38ce1cc55c65e2f5568a60bd7670feb
llShtormll <149567370+llShtormll@users.noreply.github.com>
2025-05-31 13:24:20 +0300

    Add project files.

---
+++ introductionToOOP/main.cpp
@@ -0,0 +1,80 @@
+﻿#include<iostream>
+
+using namespace std;
+class Point
+{
+	double x;
+	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this-> x = x;
+	}
+	void set_y(double y)
+	{
+		this-> y = y;
+	}
+	//             Constructors:
+	/*Point()
+	{
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Point(int x)
+	{
+		this->x = x;
+		this->y = 0;
+		cout << "constructor:\t\t" << this << endl;
+	}*/
+	Point(double x = 0, double y = 0)
+	{
+		this->x = x;
+		this->y = y;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+
+	//           Metods:
+	void print()const
+	{
+		cout << "X = " << get_x() << "\tY = " << get_y() << endl;
+	}
+};
+//#define STRUCT_POINT
+void main()
+
+{
+	setlocale(LC_ALL, "");
+
+#ifdef STRUCT_POINT
+	int a; //Обьявление переменной 'a' типа 'int'
+	Point A; //Обьявление переменной 'A' типа 'Point' 
+	//Создание обьекта (экземпляр) 'A' структуры 'Point'
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+	Point* pA = &A;
+	cout << pA->x << '\t' << pA->y << endl;
+#endif // STRUCT_POINT
+
+	Point A;
+	//A.set_y(3); 
+	//A.set_x(2);
+	//std::cout << A.get_x() << "\t" << A.get_y() << endl;
+	A.print();
+	Point B = 5;
+	//cout << B.get_x() << "\t" << B.get_y() << endl;
+	B.print();
+	Point C(7, 8);
+	C.print();
+}
